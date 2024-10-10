3d4a10ce9562b9cab558ae25eebd7eed28fc3998
ÁlvaroFGA <alvarofredgonza2020@gmail.com>
2024-10-10 13:50:09 -0400

    detalles de la eliminacion logica de los roles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import NavComponent from './components/NavComponent';
+import RolesComponent from './components/RolesComponent';
 import LoginComponent from './components/LoginComponent';
 import UsuariosComponent from './components/UsuarioComponent';  // Otros componentes que necesites
 
@@ -26,6 +27,8 @@ function App() {
                     <Routes>
                         <Route path="/login" element={<LoginComponent />} />
                         <Route path="/usuarios" element={<UsuariosComponent />} />
+                        <Route path="/roles" element={<RolesComponent />} />
+
                         {/* Agrega más rutas si es necesario */}
                     </Routes>
                 </Layout>
